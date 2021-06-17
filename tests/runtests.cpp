f57d98876af75944a6754a2380f4c46cf4891b9a
Adam Hedges <achedges@gmail.com>
2021-06-17 10:38:17 -0500

    Removed newlines from test headers

--- tests/runtests.cpp
+++ tests/runtests.cpp
@@ -8,14 +8,14 @@
 int main() {
 	int failcnt = 0;
 
-	std::cout << std::endl << "Running tokenizer tests" << std::endl;
+	std::cout << "Running tokenizer tests" << std::endl;
 	failcnt += testTokenStream();
 
-	std::cout << std::endl << "Running parser tests" << std::endl;
+	std::cout << "Running parser tests" << std::endl;
 	failcnt += testArrayParser();
 	failcnt += testObjectParser();
 
-	std::cout << std::endl << "Running serializer tests" << std::endl;
+	std::cout << "Running serializer tests" << std::endl;
 	failcnt += testJsonArray();
 	failcnt += testJsonBoolean();
 	failcnt += testJsonFloat();
